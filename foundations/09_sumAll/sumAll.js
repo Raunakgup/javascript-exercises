33d8f686436f9d390aeed8de16d2f9f2898bc42e
Raunak Gupta <graunak424@gmail.com>
2025-12-20 11:34:49 +0530

    Solve array related exercises

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(typeof a !== "number" || typeof b !== "number") return "ERROR";
+    if(isNaN(a) || isNaN(b)) return "ERROR";
+    let aa=Math.min(a,b);
+    let bb=Math.max(a,b);
+    a=aa; b=bb;
+    if(a<0 || Math.floor(a)!==a || Math.floor(b)!==b) return "ERROR";
+    let ans=0;
+    for(let i=a; i<=b; i++){
+        ans+=i;
+    }
+    return ans;
 };
 
 // Do not edit below this line
